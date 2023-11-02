0f9e4af7bcbd1676c5bb13c655f9dc491bee0b5d
cas25a <147539206+cas25a@users.noreply.github.com>
2023-11-02 16:37:26 -0600

    se hacen cambios

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,61 @@
-<script setup>
-
-</script>
-
 <template>
-  <v-form>
-    <v-container>
+  <v-container>
+    <v-alert :text="mensajeAlerta" type="success" v-if="alerta"></v-alert>
+    <br />
+    <v-form @submit.prevent="agregarDatos(datos)">
       <v-row>
         <v-col cols="12" md="6">
-          <v-text-field label="Nombre"></v-text-field>
+          <v-text-field label="Nombre" v-model="datos.nombre"></v-text-field>
         </v-col>
         <v-col cols="12" md="6">
-          <v-text-field label="Edad"></v-text-field>
+          <v-text-field label="Edad" v-model="datos.edad"></v-text-field>
         </v-col>
         <v-col cols="12" md="6">
-          <v-text-field label="Pais"></v-text-field>
+          <v-text-field label="Pais" v-model="datos.pais"></v-text-field>
         </v-col>
         <v-col cols="12" md="6">
-          <v-text-field label="Estado"></v-text-field>
+          <v-text-field label="Estado" v-model="datos.estado"></v-text-field>
         </v-col>
         <v-col cols="12" md="6">
-          <v-text-field label="Ciudad"></v-text-field>
+          <v-text-field label="Ciudad" v-model="datos.ciudad"></v-text-field>
         </v-col>
       </v-row>
-      <v-btn>Enviar</v-btn>
-    </v-container>
-  </v-form>
+      <v-btn type="submit">Enviar</v-btn>
+    </v-form>
+  </v-container>
 </template>
-
+<script>
+export default {
+  data() {
+    return {
+      datos: [],
+      alerta: false,
+      mensajeAlerta: "",
+    };
+  },
+  methods: {
+    alertaT() {
+      this.alerta = true;
+      setTimeout(() => {
+        this.alerta = false;
+      }, "3000");
+    },
+    agregarDatos(item) {
+      // console.log('ya se envio', item)
+      this.alertaT();
+      this.axios
+        .post("/save", item)
+        .then((res) => {
+          this.alertaT();
+          this.mensajeAlerta = res.data.mensaje;
+          this.datos = [];
+          console.log(item);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    },
+  },
+};
+</script>
 <style scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,15 +1,19 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import axios from "axios";
+import VueAxios from "vue-axios";
+import { createApp } from "vue";
+import App from "./App.vue";
 
 // Vuetify 3
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
+axios.defaults.baseURL = "http://localhost:3000/api";
 
 const vuetify = createVuetify({
-    components,
-    directives,
-  })
+  components,
+  directives,
+});
 
-createApp(App).use(vuetify).mount('#app')
+createApp(App).use(vuetify).use(VueAxios, axios).mount("#app");
