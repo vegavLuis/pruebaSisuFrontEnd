10a88d375e73f97a72c3205c4e82a52bc6b1354f
cas25a <147539206+cas25a@users.noreply.github.com>
2023-11-02 10:36:13 -0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+
+</script>
+
+<template>
+  <v-form>
+    <v-container>
+      <v-row>
+        <v-col cols="12" md="6">
+          <v-text-field label="Nombre"></v-text-field>
+        </v-col>
+        <v-col cols="12" md="6">
+          <v-text-field label="Edad"></v-text-field>
+        </v-col>
+        <v-col cols="12" md="6">
+          <v-text-field label="Pais"></v-text-field>
+        </v-col>
+        <v-col cols="12" md="6">
+          <v-text-field label="Estado"></v-text-field>
+        </v-col>
+        <v-col cols="12" md="6">
+          <v-text-field label="Ciudad"></v-text-field>
+        </v-col>
+      </v-row>
+      <v-btn>Enviar</v-btn>
+    </v-container>
+  </v-form>
+</template>
+
+<style scoped></style>
